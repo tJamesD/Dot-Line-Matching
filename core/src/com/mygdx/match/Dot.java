9dc00e1a8e012fa9a060dd02018672092ae393af
ToasTeR <Karl.H.Marx1883@gmail.com>
2023-06-17 15:02:51 -0400

    "working pre adding 2nd screen"

---
+++ core/src/com/mygdx/match/Dot.java
@@ -0,0 +1,11 @@
+package com.mygdx.match;
+
+import com.badlogic.gdx.math.Circle;
+
+public class Dot {
+
+    Circle dot;
+    int x;
+    int y;
+
+}
---
+++ desktop/src/com/mygdx/match/DesktopLauncher.java
@@ -0,0 +1,18 @@
+package com.mygdx.match;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.mygdx.match.Match;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+
+		config.setTitle("MATCH");
+		config.setWindowedMode(400, 600);
+		config.useVsync(true);
+		config.setForegroundFPS(60);
+		new Lwjgl3Application(new Match(), config);
+	}
+}
